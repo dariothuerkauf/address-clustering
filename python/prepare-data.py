03a5c299de4a0d273e98d5230adbf77921280110
Dario Thürkauf <71887257+dariothuerkauf@users.noreply.github.com>
2023-07-24 15:54:52 +0200

    Minor changes to prepare-data.py and etherscan-api notebook

--- python/prepare-data.py
+++ python/prepare-data.py
@@ -5,38 +5,30 @@ users = pd.read_csv('../data/subsets.csv')
 polygon_addresses = users.loc[users['Polygon'] == 1, 'Address'].tolist()
 ethereum_addresses = users.loc[users['Ethereum'] == 1, 'Address'].tolist()
 
-## ERC-721
-
+"""ERC-721"""
 # Ethereum
-# Create isSetFrom and isSetTo column
 ethereum_721 = pd.read_csv('../data/ethereum_erc721.csv')
 ethereum_721['isSetFrom'] = ethereum_721['from'].isin(ethereum_addresses)
 ethereum_721['isSetTo'] = ethereum_721['to'].isin(ethereum_addresses)
 ethereum_721 = ethereum_721.drop_duplicates()
-
 # Polygon
-# Create isSetFrom and isSetTo column
 polygon_721 = pd.read_csv('../data/polygon_erc721.csv')
 polygon_721['isSetFrom'] = polygon_721['from'].isin(polygon_addresses)
 polygon_721['isSetTo'] = polygon_721['to'].isin(polygon_addresses)
 polygon_721 = polygon_721.drop_duplicates()
 
 erc721 = pd.concat([ethereum_721, polygon_721], ignore_index=True)
-
 if len(erc721) == len(polygon_721) + len(ethereum_721):
     print('ERC-721: Length check passed')
 
-## ERC-20
 
+"""ERC-20"""
 # Ethereum
-# Create isSetFrom and isSetTo column
 ethereum_20 = pd.read_csv('../data/ethereum_erc20.csv')
 ethereum_20['isSetFrom'] = ethereum_20['from'].isin(ethereum_addresses)
 ethereum_20['isSetTo'] = ethereum_20['to'].isin(ethereum_addresses)
 ethereum_20 = ethereum_20.drop_duplicates()
-
 # Polygon
-# Create isSetFrom and isSetTo column
 polygon_20 = pd.read_csv('../data/polygon_erc20.csv')
 polygon_20['isSetFrom'] = polygon_20['from'].isin(polygon_addresses)
 polygon_20['isSetTo'] = polygon_20['to'].isin(polygon_addresses)
@@ -53,17 +45,13 @@ if len(erc20) == len(ethereum_20) + len(polygon_20):
     print('ERC-20: Length check passed')
 
 
-## ERC-1155
-
+"""ERC-1155"""
 # Ethereum
-# Create isSetFrom and isSetTo column
 ethereum_1155 = pd.read_csv('../data/ethereum_erc721.csv')
 ethereum_1155['isSetFrom'] = ethereum_1155['from'].isin(ethereum_addresses)
 ethereum_1155['isSetTo'] = ethereum_1155['to'].isin(ethereum_addresses)
 ethereum_1155 = ethereum_1155.drop_duplicates()
-
 # Polygon
-# Create isSetFrom and isSetTo column
 polygon_1155 = pd.read_csv('../data/polygon_erc1155.csv')
 polygon_1155['isSetFrom'] = polygon_1155['from'].isin(polygon_addresses)
 polygon_1155['isSetTo'] = polygon_1155['to'].isin(polygon_addresses)
@@ -79,7 +67,6 @@ if len(erc1155) == len(ethereum_1155) + len(polygon_1155):
 ## Concatenate the three DataFrames and check length
 events = pd.concat([erc20, erc721, erc1155], ignore_index=True)
 print(f'Length of all events: {len(events)}')
-
 if len(events) == len(erc20) + len(erc721) + len(erc1155):
     print('Events: Length check passed')
 
